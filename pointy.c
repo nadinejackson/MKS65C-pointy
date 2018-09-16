efd68190d2326861896314497dc3eb8e7329666a
nadinejackson <njackson@stuy.edu>
2018-09-16 14:31:40 -0400

    printed bytes, made neater

--- pointy.c
+++ pointy.c
@@ -3,22 +3,31 @@ int main()
 {
   unsigned int tester = 3456789123;
   char * pointy = &tester;
-  printf("this is the int in hex: %x\n", tester);
-  printf("this is the int in decimal: %u\n", tester);
-  printf("this is the first byte in storage %hhx\n", *pointy);
+  
   int i;
   char * m = pointy;
+
+  printf("this is the int in hex: %x\n", tester);
+  printf("this is the int in decimal: %u\n", tester);
   for (i = 0; i < 4; i++){
-    *m += 1;
-    printf("this is the int in hex: %x\n", tester);
-    printf("this is the int in decimal: %u\n", tester);
+    printf("this is byte %d in hex: %hhx\n", i,  *m);
+    printf("this is byte %d in decimal: %hhu\n", i, *m);
     m ++;
   }
+  printf("~~~~~~~~~~~~~~~~~~ADDING ONE ~~~~~~~~~~~~~~~~~\n");
   m = pointy;
   for (i = 0; i < 4; i++){
-    *m += 16;    
-    printf("this is the int in hex: %x\n", tester);
-    printf("this is the int in decimal: %u\n", tester);
+    *m += 1;
+    printf("this is byte %d in hex: %hhx\n", i, *m);
+    printf("this is byte %d in decimal: %hhu\n", i, *m);
+    m ++;
+  }
+  printf("~~~~~~~~~~~~~~~~~~ADDING 16~~~~~~~~~~~~~~~~~~\n");
+  m = pointy;
+  for (i = 0; i < 4; i++){
+    *m += 16;
+    printf("this is byte %d in hex: %hhx\n", i, *m);
+    printf("this is byte %d in decimal: %hhu\n", i, *m);
     m++;}
   return 0;
 }
